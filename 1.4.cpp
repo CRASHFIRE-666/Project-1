f039c5d407377c9a4ede135313c413ac6b8680a1
CRASHFIRE666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-24 22:15:25 +0300

    Update 1.4.cpp

--- 1.4.cpp
+++ 1.4.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 int main()
 {
-setlocale( LC_ALL,"Russian" );
-double a, b;
-cout << "Введите числа через пробел: ";
-cin >> a >> b;
-a = a*a; b = b*b;
-cout << endl << "Сумма: " << a + b;
-cout << endl << "Разность: " << a - b;
-cout << endl << "Произведение: "<< a*b;
-cout << endl << "Частное: "<< a/b;
-return 0;
+  
+    setlocale( LC_ALL,"Russian" );
+    double a, b;
+    cout << "Введите числа через пробел: ";
+    cin >> a >> b;
+    a = a*a; b = b*b;
+    cout << endl << "Сумма= " << a + b;
+    cout << endl << "Разность= " << a - b;
+    cout << endl << "Произведение= "<< a*b;
+    cout << endl << "Частное= "<< a/b;
+    return 0;
 }
